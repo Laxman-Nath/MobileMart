cf8b6689b701639bfd045512427069f68fe67bb5
Laxman-Nath <nathlaxman242@gmail.com>
2024-11-21 17:05:38 +0545

    solved admin and user issues

---
+++ src/main/java/com/ecommerce/commonutils/GetLoggedInUserUtils.java
@@ -0,0 +1,5 @@
+package com.ecommerce.commonutils;
+
+public class GetLoggedInUserUtils {
+
+}
--- src/main/java/com/ecommerce/controllers/AboutController.java
+++ src/main/java/com/ecommerce/controllers/AboutController.java
@@ -1,11 +1,39 @@
 package com.ecommerce.controllers;
 
+import java.security.Principal;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import com.ecommerce.models.Category;
+import com.ecommerce.models.Customer;
+import com.ecommerce.models.Order;
+import com.ecommerce.services.CategoryService;
+import com.ecommerce.services.CustomerService;
+import com.ecommerce.services.OrderService;
+
+import lombok.RequiredArgsConstructor;
 
 @Controller
+@RequiredArgsConstructor
 public class AboutController {
-
+	private final CategoryService categoryService;
+	private final OrderService orderService;
+	private final CustomerService customerService;
+	@ModelAttribute
+	public void getLoggedInUser(Principal p, Model m) {
+		List<Category> categories = categoryService.findByIsActiveTrue();
+	
+		m.addAttribute("categories", categories);
+		String emailString = p.getName();
+		Customer customer = customerService.findByEmail(emailString);
+		List<Order> orders = this.orderService.findByCustomer(customer);
+		m.addAttribute("orders", orders);
+		m.addAttribute("loggedUser", customer);
+	}
 	@GetMapping("/about")
 	public String getAbout() {
 		return "aboutus";
